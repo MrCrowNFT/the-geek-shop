a05d5a79342c5b047d652a5c005b709ef3561d32
MrCrowNFT <mrcrownft@gmail.com>
2025-03-06 19:15:36 -0300

    App

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import Stripe from "stripe";
+
+const app = express();
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+app.use(express.json()); //accept JSON files
+app.use(cors()); // Allow all origins: probably should change it just specific one
+
+app.use("/home", userRouter);
+app.use("/admin", adminRouter);
+app.use("/categories", categoryRouter);
+
+app.post("/create-payment-intent", async (req, res) => {
+  try {
+    const { amount, currency } = req.body;
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency,
+    });
+
+    res.json({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+export default app;
