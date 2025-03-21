5e56a77f430bd4425753b7ed0673a1393e811df5
MrCrowNFT <mrcrownft@gmail.com>
2025-03-21 09:43:18 -0300

    trackig info api calls

--- frontend/src/api/services/tracking.ts
+++ frontend/src/api/services/tracking.ts
@@ -1,1 +1,42 @@
+import api from "../axios";
 
+export const newTracking = async (newTrackingInfo: {
+  orderId: string;
+  carrier: string;
+  trackingNumber: string;
+}) => {
+  try {
+    const res = await api.post("/tracking/", newTrackingInfo);
+    return res.data;
+  } catch (err) {
+    console.error("Creating tracking info error:", err);
+    throw err;
+  }
+};
+
+export const getTrackingInfo = async (trackingId: string) => {
+  try {
+    const res = await api.get(`/tracking/${trackingId}`);
+    return res.data;
+  } catch (err) {
+    console.error("Fetching tracking info error:", err);
+    throw err;
+  }
+};
+
+export const updateTrackingInfo = async (
+  trackingId: string,
+  updatedTrackingInfo: {
+    orderId: string;
+    carrier: string;
+    trackingNumber: string;
+  }
+) => {
+  try {
+    const res = await api.put(`/tracking/${trackingId}`, updatedTrackingInfo);
+    return res.data;
+  } catch (err) {
+    console.error("Updting tracking info error:", err);
+    throw err;
+  }
+};
