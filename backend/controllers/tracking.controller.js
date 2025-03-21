6df6cf74618a72942026faf9aa80eb092132e39d
MrCrowNFT <mrcrownft@gmail.com>
2025-03-21 07:15:26 -0300

    Tracking info controllet

---
+++ backend/controllers/tracking.controller.js
@@ -0,0 +1,98 @@
+import Tracking from "../module/tracking.model";
+import mongoose from "mongoose";
+
+/**
+ * Crates a new tracking document and adds it to the order
+ * @route POST /tracking
+ * @access Private
+ */
+export const createTracking = async (req, res) => {
+  try {
+    const { orderId, carrier, trackingNumber } = req.body;
+
+    if (!orderId | !carrier | !trackingNumber) {
+      return res
+        .status(400)
+        .json({ message: "OrderId, carrier and trackingNumber needed" });
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(orderId)) {
+      return res.status(400).json({ message: "Invalid order ID format" });
+    }
+
+    const newTracking = new Tracking({
+      order: orderId,
+      carrier,
+      trackingNumber,
+      estimatedDelivery,
+    });
+    //todo need the function that updates order and reference new tracking
+    const savedTracking = await newTracking.save();
+    res.status(201).json(savedTracking);
+  } catch (error) {
+    console.error("Error creating tracking:", error);
+    res.status(500).json({
+      message: "Error creating tracking",
+      error: error.message,
+    });
+  }
+};
+
+export const getTrackingById = async (req, res) => {
+  try {
+    const trackingId = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(trackingId)) {
+      return res.status(400).json({ message: "Invalid tracking ID format" });
+    }
+
+    const tracking = Tracking.findById(trackingId);
+
+    res.status(201).json(tracking);
+  } catch (error) {
+    console.error("Error fetching tracking:", error);
+    res.status(500).json({
+      message: "Error fetching tracking",
+      error: error.message,
+    });
+  }
+};
+export const updateTracking = async (req, res) => {
+  try {
+    const trackingId = req.params.id;
+    const updateData = req.body;
+
+    if (!trackingId || !updateData) {
+      return res.status(400).json({ success: false, message: "Invalid input" });
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(trackingId)) {
+      return res.status(400).json({ message: "Invalid tracking ID format" });
+    }
+    const trackingToUpdate = await Product.findById(trackingId);
+
+    if (!trackingToUpdate) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Tracking info not found" });
+    }
+    Object.keys(updateData).forEach((key) => {
+      trackingToUpdate[key] = updateData[key];
+    });
+
+    //todo make sure this doesn't re calculate the tracking
+    const updatedTrackingInfo = await trackingToUpdate.save();
+    return res.status(200).json({ success: true, data: updatedTrackingInfo });
+  } catch (error) {
+    console.error(`Error updating tracking info: ${error.message}`);
+    if (error.name === "CastError") {
+      return res
+        .status(400)
+        .json({ success: false, message: "Tracking info product ID" });
+    }
+    return res
+      .status(500)
+      .json({ success: false, message: error.message || "Server error" });
+  }
+};
+//there is no need for a delete tracking one
--- backend/module/tracking.model.js
+++ backend/module/tracking.model.js
@@ -1,18 +1,28 @@
 import mongoose from "mongoose";
 
-//TODO build the controller and router for this, should only be accesible for the admin
-//this is up for change
-const trackingSchema = new mongoose.Schema({
-  tracking_number: {
-    type: Number,
-    required: true,
+const trackingSchema = new mongoose.Schema(
+  {
+    order: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+      required: true,
+    },
+    carrier: {
+      type: String, 
+      required: true,
+    },
+    trackingNumber: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    estimatedDelivery: {
+      type: Date,
+    },
   },
-  order: {
-    order: mongoose.Schema.Types.ObjectId,
-    ref: "Order",
-    required: true,
-  },
-});
+  { timestamps: true }
+);
+//todo, add a pre save hook that calculates the estimated delivery
 
 const Tracking = mongoose.model("Tracking", trackingSchema);
 export default Tracking;
