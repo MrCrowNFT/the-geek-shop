95b8491342f90870b4f162ca6080dba6c1598927
MrCrowNFT <mrcrownft@gmail.com>
2025-03-24 08:04:46 -0300

    use create order

--- frontend/src/hooks/use-order.ts
+++ frontend/src/hooks/use-order.ts
@@ -1,5 +1,5 @@
-import { fetchUserOrders } from "@/api/services/order";
-import { useQuery } from "@tanstack/react-query";
+import { createOrder, fetchUserOrders } from "@/api/services/order";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const useFetchUserOrders = () => {
   try {
@@ -9,11 +9,27 @@ export const useFetchUserOrders = () => {
       staleTime: 1000 * 60 * 5,
       retry: 2,
     });
-    return data
+    return data;
   } catch (err) {
     console.error("Caching or refetching orders error:", err);
     throw err;
   }
 };
 
-
+export const useCreateOrder = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (orderData: {
+      shipping: string;
+      products: { id: string; quantity: number }[];
+      paid_amount: number;
+    }) => createOrder(orderData),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["order"] });
+    },
+    onError: (error) => {
+      console.error("Create order mutation error:", error);
+      throw error;
+    },
+  });
+};
