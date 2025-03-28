f98138305e08c9d637d6102bc9028ecf8078bdb7
MrCrowNFT <mrcrownft@gmail.com>
2025-03-28 06:32:02 -0300

    pages folder updared

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Home from "./pages/home";
-import ProductPage from "./pages/product";
-import Login from "./pages/login";
-import Signup from "./pages/signup";
-import Dashboard from "./pages/dashboard";
-import OrderDetailsPage from "./pages/order";
-import Search from "./pages/search";
+import Home from "./pages/common/home";
+import ProductPage from "./pages/common/product";
+import Login from "./pages/common/login";
+import Signup from "./pages/common/signup";
+import Dashboard from "./pages/admin/dashboard";
+import OrderDetailsPage from "./pages/admin/order";
+import Search from "./pages/common/search";
 
 const queryClient = new QueryClient();
 
--- frontend/src/components/admin/product-details.tsx
+++ frontend/src/components/admin/product-details.tsx
@@ -1,1 +1,2 @@
-//TODO admin product details
+//TODO admin product details, should be able to see details of product of given id,
+//todo: (should get the id from params) and also update product from here 
