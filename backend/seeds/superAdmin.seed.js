2e369927b9a92e591bf4c19dd3fab81c7e1c8254
MrCrowNFT <mrcrownft@gmail.com>
2025-04-15 09:19:45 -0400

    Super admin seed

---
+++ backend/seeds/superAdmin.seed.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import User from "../models/user.model";
+import connectDb from "../config/db";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const seedSuperAdmin = async () => {
+  try {
+    const existingSuperAdmin = await User.findOne({ role: "super_admin" });
+    if (existingSuperAdmin) {
+      console.log("Super Admin already exists");
+      process.exit(0);
+    }
+
+    const superAdmin = new User({
+      username: process.env.SUPER_ADMIN_USERNAME,
+      email: process.env.SUPER_ADMIN_EMAIL,
+      password: process.env.SUPER_ADMIN_PASSWORD,
+      role: "super_admin",
+    });
+    await superAdmin.save();
+    console.log("Super admin created succesfully");
+    process.exit(0);
+  } catch (error) {
+    console.error("Error seeding data:", error.message);
+    console.error(error.stack);
+    process.exit(1);
+  } finally {
+    await mongoose.disconnect();
+    console.log("Database connection closed.");
+  }
+};
+
+const runSeed = async () => {
+  try {
+    await connectDb();
+    await seedSuperAdmin();
+  } catch (error) {
+    console.error("Failed to connect to database:", error);
+    process.exit(1);
+  }
+};
+
+runSeed();
